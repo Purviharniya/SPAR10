e515596fe46f28f0f3d663bf46dbfd711bf945ab
unknown <purvi.harniya@gmail.com>
2021-12-03 15:03:30 +0530

    update profile

--- __init__.py
+++ __init__.py
@@ -14,7 +14,7 @@ def create_app():
     app.config['MAIL_SERVER']='smtp.gmail.com'
     app.config['MAIL_PORT'] = 465
     app.config['MAIL_USERNAME'] = 'spar10.project@gmail.com'
-    app.config['MAIL_PASSWORD'] = '[password]'
+    app.config['MAIL_PASSWORD'] = 'SPAR10PROJECT'
     app.config['MAIL_USE_TLS'] = False
     app.config['MAIL_USE_SSL'] = True
     postamail = Mail(app)
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template,redirect, url_for, request, flash,session
 # from . import
 from flask_login import login_required, current_user
 from __init__ import create_app,db,postamail
+import re
 
 main = Blueprint('main', __name__)
 
@@ -46,10 +47,51 @@ def textclassification():
 def textredaction():
     return render_template('textredaction.html')
 
-@main.route("/profile")
+def validate_profile_details(name,email,contact):
+
+    if name=='' or email =='' or contact =='':
+        return("Please fill all the fields!")
+    
+    if not re.fullmatch(re.compile(r'^[a-zA-Z ]+$'), name):
+        return('Invalid name')
+
+    if not re.fullmatch(re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+'), email):
+        return('Invalid email address')
+       
+    return True
+
+@main.route("/profile",methods=["POST","GET"])
 @login_required
 def profile():
-    return render_template('profile.html', name=current_user.name)
+    if request.method=='POST':
+        name = request.form['name']
+        mail = request.form['email']
+        contact = request.form['contact']
+        id = current_user.id
+
+        check_valid = validate_profile_details(name,mail,contact)
+        if check_valid != True:
+            flash(check_valid,"error")
+            return redirect(url_for('main.profile'))
+
+        try:
+            db.session.commit()
+            flash(u"Details updated successfully!","success")
+            current_user.name = name
+            current_user.email = mail
+            current_user.contact = contact
+            return render_template('profile.html', name=current_user.name , user = current_user)
+        except:
+            flash(u"Looks like there is some error with database!","error")
+            return render_template('profile.html', name=current_user.name , user = current_user)
+
+    else:    
+        return render_template('profile.html', name=current_user.name , user = current_user)
+
+# @main.route("/settings",methods=["POST","GET"])
+# @login_required
+# def settings():
+
 
 @main.route("/dashboard")
 @login_required
