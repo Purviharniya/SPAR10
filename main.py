4d8853798f8e4b5882625a7ea443b54315fba1f5
Akhil Namboodiri <akhil.namboodiri@somaiya.edu>
2022-04-26 10:25:41 +0530

    sidebar and extraction result fxed

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from __init__ import create_app,db
 
 if __name__ == '__main__':
     db.create_all(app=create_app()) # create the SQLite database
-    create_app().run(debug=True) # run the flask app on debug mode
+    create_app().run(debug=True,port=10000) # run the flask app on debug mode
